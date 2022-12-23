8114b9eb4377dc0b9081b33231d51a5f5b50f96d
prasadch <id>
2022-12-23 14:05:35 +0530

    a

---
+++ five.java
@@ -0,0 +1,11 @@
+public static
+
+{
+
+}
+
+10th day how devolaper will push/upload into the github account.
+
+git remote add origin https://github.com/crazyprasad/repo20.git
+
+git push -u origin master
